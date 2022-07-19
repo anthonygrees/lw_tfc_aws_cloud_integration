2732d20b6bfecc467db15696bb07c6e542213bf8
Anthony Rees <anthony_rees01@hotmail.com>
2022-07-19 12:00:00 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "my-account" {
+  type    = string
+}
+
+variable "my-api-key" {
+  type    = string
+}
+
+variable "my-api-secret" {
+  type    = string
+}
+
+variable "bucket-arn" {
+  type    = string
+}
